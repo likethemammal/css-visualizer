05b87280d0fb26d7ff3e05bcd47610944dc8f058
Likethemammal <cdolphin1991@gmail.com>
2016-03-06 14:56:01 -0500

    Repo cleanup

--- server.js
+++ server.js
@@ -1,17 +1,10 @@
-/* Express 3 requires that you instantiate a `http.Server` to attach socket.io to first */
 var express = require('express'),
     app = express(),
     server = require('http').createServer(app),
     io = require('socket.io').listen(server),
     uuid = require('node-uuid'),
-    port = 3000,
+    port = 4000,
     url  = 'http://localhost:' + port + '/';
-
-/* We can access nodejitsu enviroment variables from process.env */
-/* Note: the SUBDOMAIN variable will always be defined for a nodejitsu app */
-if(process.env.SUBDOMAIN){
-    url = 'http://' + process.env.SUBDOMAIN + '.jit.su/';
-}
 
 server.listen(port);
 console.log("Express server listening on port " + port);
